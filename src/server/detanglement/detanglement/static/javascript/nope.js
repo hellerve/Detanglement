a11c86b1d293ad67dd8658e8af3058cbd5deda8e
Veit Heller <s0539501@htw-berlin.de>
2014-06-25 13:28:31 +0200

    Updated styles

---
+++ src/server/detanglement/detanglement/static/javascript/nope.js
@@ -0,0 +1,3 @@
+toastr.error("Your browser does not support SVG. It is needed for rendering this page.", 
+             "Compatibility error");
+toastr.warning("A static version of this page will be displayed.", "Notification");
