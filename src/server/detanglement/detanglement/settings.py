06ddc675806853cdb1f59082634c49cd6dcaeba1
Veit Heller <veit.heller@student.htw-berlin.de>
2014-08-01 18:00:17 +0200

    Finished user management for now

--- src/server/detanglement/detanglement/settings.py
+++ src/server/detanglement/detanglement/settings.py
@@ -110,5 +110,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+        os.path.join(BASE_DIR, 'static/').replace('\\', '/'),
 )
--- src/server/detanglement/detanglement/urls.py
+++ src/server/detanglement/detanglement/urls.py
@@ -1,17 +1,32 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.core.exceptions import PermissionDenied
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
+def login():
+    return redirect('/accounts/login/')
+
+def profile():
+    return PermissionDenied()
+
 urlpatterns = patterns('',
     url(r'^admin/?', include(admin.site.urls)),
+    url(r'^login/?', 'datavis.views.auth_check',
+        {'fun': login}),
     url(r'^accounts/?', include('registration.backends.default.urls')),
-    url(r'^login/?', 'datavis.views.redir',
-        {'site': '/accounts/login/'}),
-    url(r'^[home]?$', 'datavis.views.serve',
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^$', 'datavis.views.serve', {'site': 'datavis/index.html'}),
+    url(r'^home/?$', 'datavis.views.serve',
         {'site': 'datavis/index.html'}),
     url(r'^settings/?', 'datavis.views.serve',
         {'site': 'datavis/settings.html'}),
     url(r'^apis/?', 'datavis.views.serve', {'site': 'datavis/apis.html'}),
-    url(r'^accounts/profile/?', 'datavis.views.profile'),
+    url(r'^accounts/profile/?', 'datavis.views.auth_check',
+        {'fun': profile}),
 )
+
+urlpatterns += staticfiles_urlpatterns()
