b74057e6372e0d5a0b8c7b3ee153c749d1cc4f62
Veit Heller <veit.heller@student.htw-berlin.de>
2014-09-01 21:15:37 +0200

    Updated nav style

--- src/server/detanglement/static/javascript/navStartup.js
+++ src/server/detanglement/static/javascript/navStartup.js
@@ -1,11 +1,13 @@
 (function() {
   $(document).ready(function() {
     $('.menu').click(function() {
+      $('.clearfix').css('background-color', '#404040');
       $('nav.right').addClass('open');
       $('body').addClass('menu-open');
       return false;
     });
     return $(document).click(function() {
+      $('.clearfix').css('background-color', 'rgba(70, 70, 70, 0.35)');
       $('body').removeClass('menu-open');
       return $('nav').removeClass('open');
     });
